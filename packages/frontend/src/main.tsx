0c4337da992e5d239b290a37af6bade76c7e18a4
Thirafi Dide <thirafi@fb.com>
2024-02-05 21:10:54 +0700

    Simple web navigation and routing

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.tsx";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -7,6 +8,8 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<App />
+		<Router>
+			<App />
+		</Router>
 	</React.StrictMode>
 );
