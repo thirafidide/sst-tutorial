dea5b12cf341f741d747d3e71d447dbd58f5c3a5
Thirafi Dide <thirafi@fb.com>
2024-02-05 21:17:27 +0700

    Login form

--- packages/frontend/src/Routes.tsx
+++ packages/frontend/src/Routes.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./containers/Home.tsx";
 import NotFound from "./containers/NotFound.tsx";
+import Login from "./containers/Login.tsx";
 
 export default function Links() {
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
-			<Route path="*" element={<NotFound />} />;
+			<Route path="/login" element={<Login />} />
+			<Route path="*" element={<NotFound />} />
 		</Routes>
 	);
 }
