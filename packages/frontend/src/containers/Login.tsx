55a0e9cfac82d6644297a5e2627341e1bbe64b02
Thirafi Dide <thirafi@fb.com>
2024-02-06 17:39:14 +0700

    Refactor form hooks

--- packages/frontend/src/containers/Login.tsx
+++ packages/frontend/src/containers/Login.tsx
@@ -9,17 +9,20 @@ import "./Login.css";
 import { useNavigate } from "react-router-dom";
 import LoaderButton from "../components/LoaderButton";
 import { onError } from "../lib/errorLib";
+import { useFormFields } from "../lib/hooksLib";
 
 export default function Login() {
 	const { userHasAuthenticated } = useAppContext();
 	const nav = useNavigate();
 
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
 	const [isLoading, setIsLoading] = useState(false);
+	const [fields, handleFieldChange] = useFormFields({
+		email: "",
+		password: "",
+	});
 
 	function validateForm() {
-		return email.length > 0 && password.length > 0;
+		return fields.email.length > 0 && fields.password.length > 0;
 	}
 
 	async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
@@ -28,7 +31,7 @@ export default function Login() {
 		setIsLoading(true);
 
 		try {
-			await Auth.signIn(email, password);
+			await Auth.signIn(fields.email, fields.password);
 			userHasAuthenticated(true);
 			nav("/");
 		} catch (error) {
@@ -48,8 +51,8 @@ export default function Login() {
 							autoFocus
 							size="lg"
 							type="email"
-							value={email}
-							onChange={(e) => setEmail(e.target.value)}
+							value={fields.email}
+							onChange={handleFieldChange}
 						/>
 					</Form.Group>
 					<Form.Group controlId="password">
@@ -57,8 +60,8 @@ export default function Login() {
 						<Form.Control
 							size="lg"
 							type="password"
-							value={password}
-							onChange={(e) => setPassword(e.target.value)}
+							value={fields.password}
+							onChange={handleFieldChange}
 						/>
 					</Form.Group>
 					<LoaderButton
